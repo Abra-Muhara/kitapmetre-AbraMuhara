d35db6a5249f4b34efa530c956746664d691fe96
Şuayp Talha Kocabay <162293855+suayptalha@users.noreply.github.com>
2024-08-06 22:43:32 +0300

    Create random_forest_code.py

---
+++ modeller/yas-aralıgı-modeli/Random Forest (Kullanılan)/random_forest_code.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import sklearn
+import tensorflow
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
+rawData = pd.read_excel("/content/Final.xlsx")
+rawY=rawData["Önerilen Yaş Aralığı"]
+rawX=(rawData.drop("Dosya Adı",axis=1)).drop("Önerilen Yaş Aralığı",axis=1)
+label_encoder = LabelEncoder()
+Y=label_encoder.fit_transform(rawY)
+X_train, X_test, y_train, y_test = train_test_split(rawX, Y, test_size=0.2, random_state=42)
+
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+print("Random Decision: ")
+rf.fit(X_train, y_train)
+
+y_pred = rf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred, target_names=label_encoder.classes_)
+
+print(f"Doğruluk: {accuracy}")
+print(report)
