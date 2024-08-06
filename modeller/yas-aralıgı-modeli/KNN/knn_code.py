a8879cbedfa405c06e1805568284a572cbfbacea
Şuayp Talha Kocabay <162293855+suayptalha@users.noreply.github.com>
2024-08-06 22:37:36 +0300

    Create knn_code.py

---
+++ modeller/yas-aralıgı-modeli/KNN/knn_code.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, accuracy_score
+
+file_path = '/content/Final.xlsx'
+data = pd.read_excel(file_path)
+
+X = data.drop(columns=['Dosya Adı', 'Önerilen Yaş Aralığı'])
+y = data['Önerilen Yaş Aralığı']
+
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train, y_train)
+
+y_pred = knn_model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred, target_names=label_encoder.classes_)
+
+print(f"Accuracy: {accuracy}")
+print("Classification Report:")
+print(report)
